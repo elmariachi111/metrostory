d9a7a724271a9e00f86f1e12825e52314e7a4dfa
Stefan <stadolf@gmail.com>
2014-06-21 22:04:17 +0200

    refactored route list / welcome screen

--- public/javascripts/Models/Routes.js
+++ public/javascripts/Models/Routes.js
@@ -4,10 +4,21 @@ MS.Models.Route = Backbone.Model.extend({
 
 MS.Models.Routes = Backbone.Collection.extend({
     model: MS.Models.Route,
+    initialize: function() {
+      this.on("localized", this.localized);
+    },
     url: function() {
         return MS.Constants.API_HOME +  "/api/getLinesForLoc";
     },
     parse: function(response) {
         return response.items;
+    },
+    localized: function(navgPos) {
+        this.fetch({data: {
+                lat: navgPos.latitude,
+                lon: navgPos.longitude,
+                radius:MS.Constants.DEFAULT_ROUTE_RADIUS},
+                reset:true}
+        );
     }
 });
--- public/javascripts/Views/WelcomeView.js
+++ public/javascripts/Views/WelcomeView.js
@@ -1,17 +1,19 @@
-MS.Views.RouteList = Backbone.View.extend({
+MS.Views.RouteSelectionView = Backbone.View.extend({
     events: {
         "tap .route": "routeSelected"
     },
     initialize: function() {
+        this.collection = new MS.Models.Routes;
+
         this.listenTo(this.collection, "reset", this.showRoutes);
         this.$tableView = this.$("ul.table-view");
     },
     showRoutes: function() {
-        this.$el.html("");
+        this.$tableView.html("");
         var self = this;
         this.collection.each( function(r)  {
             var html = '<li class="table-view-cell route" data-line="'+ r.get('line') +'">'+ r.get('line')+'</li>';
-            self.$el.append( $(html) );
+            self.$tableView.append( $(html) );
         });
     },
     routeSelected: function(evt) {
@@ -27,22 +29,11 @@ MS.Views.WelcomeView = Backbone.View.extend({
     },
     initialize: function(options) {
         this.messageList = options.messageList;
-        this.routeChoices = new MS.Models.Routes;
-        this.routeList = new MS.Views.RouteList( {
-            collection: this.routeChoices,
-            el: this.$('#route-list')
-        });
-
+        this.routeView = options.routeView;
     },
     localized: function(navgPos) {
+        this.routeView.collection.trigger("localized", navgPos.coords);
 
-        this.trigger("localized", navgPos.coords);
-        this.routeChoices.fetch({data: {
-                lat: navgPos.coords.latitude,
-                lon: navgPos.coords.longitude,
-                radius:MS.Constants.DEFAULT_ROUTE_RADIUS},
-                reset:true}
-        );
     },
     routeSelected: function(route) {
         this.messageList.fetch({data: navgPos.coords, reset:true});
