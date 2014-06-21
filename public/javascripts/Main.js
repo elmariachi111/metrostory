b13f3f712fa21d066c75f9530595f30fe423fe58
Stefan <stadolf@gmail.com>
2014-06-21 23:15:25 +0200

    wrong api loc

--- public/javascripts/Main.js
+++ public/javascripts/Main.js
@@ -2,7 +2,7 @@ MS = {
     Views: {},
     Models: {},
     Constants: {
-        API_HOME: "http://localhost:3000",
+        API_HOME: "http://metrostory.cloudcontrolapp.com",
         DEFAULT_ROUTE_RADIUS: 1500,
         MESSAGE_THROTTLE: 15000 //poll every 15s
     }
