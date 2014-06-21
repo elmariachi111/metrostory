274dec83e8247c6770f91ae9b0f537eef7cbf213
RobTS <robert@tochman-szewc.de>
2014-06-21 20:04:07 +0200

    location.js

--- routes/location.js
+++ routes/location.js
@@ -2,18 +2,22 @@ var locationSvc = {};
 var express = require('express');
 var router = express.Router();
 var db = require('../db.js');
-
+var UND = require('underscore');
 /* GET home page. */
 
 locationSvc.getLines = function (req, res) {
+    var lat = req.param("lat"); //52.66
+    var lon = req.param("lon"); //13.58
+    var distance = req.param("min_dst");//1000
+
     var v = db.collection('veebibi');
 
     v.find({ "stations.loc": {
         $near: {
             $geometry: {
                 type: "Point",
-                coordinates: [ 13.58 , 52.66 ] },
-            $maxDistance: 1000
+                coordinates: [ lon , lat] },
+            $maxDistance: distance
         }
     }
     }).limit(100).toArray(function (err, items) {
@@ -22,11 +26,32 @@ locationSvc.getLines = function (req, res) {
     });
 };
 
+
 locationSvc.getDataForRoute = function (req, res) {
+    var line = req.param("line");
+
+    db.collection('veebibi').find({"line": line}).toArray(function (err, items) {
+        var x = 0;
+        var val = 0;
+        if (items)
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].stations.length > val) {
+                    val = items[i].stations.length;
+                    x = i;
+                }
+            }
+        else
+        {res.json({ items: "error"});
+        return;}
+        res.json({ items: items[x].stations});
+
+
+    });
 
 };
 
 router.get('/getLinesForLoc', locationSvc.getLines);
+router.get('/getDataForRoute', locationSvc.getDataForRoute);
 
 
 module.exports = router;
