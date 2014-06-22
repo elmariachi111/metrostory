bed4ca84366f922e4ecd42413b31d69fab95500e
Stefan <stadolf@gmail.com>
2014-06-22 02:01:25 +0200

    get nearest tweets by station twitter rendering + Handlebars, moment

--- public/javascripts/Models/Messages.js
+++ public/javascripts/Models/Messages.js
@@ -1,3 +1,25 @@
+MS.Models.Station = Backbone.Model.extend({
+    latLng: function() {
+        var loc = this.get('loc');
+        return {
+            latitude: loc.lat ,
+            longitude: loc.lng
+        };
+    }
+
+});
+
+MS.Models.StationCollection = Backbone.Collection.extend({
+    model: MS.Models.Station,
+    findClosestStation: function(latlng) {
+        var nearestStation = this.min( function( station ) {
+            return MS.Haversine(latlng, station.latLng());
+        });
+
+        return nearestStation;
+    }
+
+});
 
 MS.Models.Message = Backbone.Model.extend({
 
@@ -5,15 +27,30 @@ MS.Models.Message = Backbone.Model.extend({
 
 MS.Models.MessageList = Backbone.Collection.extend({
     model: MS.Models.Message,
-
-    url: function() {
-        return MS.Constants.API_HOME + "/api/messages"
+    initialize: function() {
+      this.stations = new MS.Models.StationCollection({})
     },
-    reload: function(route) {
-        this.fetch({data: {route:route}, reset:true});
+    url: function() {
+        return MS.Constants.API_HOME + "/api/getDataForRoute"
+    },
+    reload: function(line) {
+        this.fetch({data: {line:line}, reset:true});
     },
     parse: function(response) {
-        return response;
+        this.stations.reset(response.stations);
+        return response.contents;
+    },
+    findContent: function(latlng) {
+       var nearestStation = this.stations.findClosestStation(latlng);
+
+       var tweetsForThatStation = this.filter( function(tweet) {
+           return _.some(tweet.get('nearStations'), function(st) {
+               return st._id==nearestStation.get('id');
+           });
+       });
+
+       return tweetsForThatStation;
+
     }
 })
 
